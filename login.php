341abaac33d4e108dda33e33e715ae7229393e3e
mamaclayyulxvans17 <69473090+mamaclayyulxvans17@users.noreply.github.com>
2022-12-02 01:52:19 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "db_conn.php";
 
 if (isset($_POST['uname']) && isset($_POST['password'])) { 
@@ -16,15 +17,32 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
     if (empty($uname)){
         header("Location: index.php?error=Username is required");
         exit();
-    } else if (empty($password)){
+    } else if (empty($pass)){
         header("Location: index.php?error=Password is required");
         exit();
-    } else{
+    }else{
+        //hashing the password 
+        $pass = md5($pass);
         $sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
 
         $result = mysqli_query($conn, $sql);
 
-        if (mysqli_num_rows)
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['user_name'] === $uname && $row['password'] === $pass){
+                $_SESSION['user_name'] = $row['user_name'];
+                $_SESSION['name'] = $row['name'];
+                $_SESSION['id'] = $row['id'];
+                header("Location: home.php");
+                exit();
+            }else{
+                header("Location: index.php?error=Incorrect User name or password");
+                exit();
+            }
+        }else{
+            header("Location: index.php?error=Incorrect User name or password");
+            exit();
+        }
 
     }
 
